822549afc21f600f2af094ebc80cd9b48f4abda4
Nikita200 <Ponomariovnikita02@gmail.com>
2026-04-09 13:54:31 +0200

    add enemy and end game if catch)

--- main.cpp
+++ main.cpp
@@ -4,13 +4,19 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "My First Game");
     sf::View view(sf::FloatRect(0, 0, 800, 600));
 
-    // squere
+    // player
     sf::RectangleShape player(sf::Vector2f(50.f, 50.f));
     player.setFillColor(sf::Color::Green);
     player.setPosition(375.f, 275.f);
 
+    //enemy
+    sf::RectangleShape enemy(sf::Vector2f(50.f, 50.f));
+    enemy.setFillColor(sf::Color::Red);
+    enemy.setPosition(100.f, 100.f);
+
+    unsigned int w_size = 2000;
     // world
-    sf::RectangleShape world(sf::Vector2f(2000.f, 2000.f));
+    sf::RectangleShape world(sf::Vector2f(w_size, w_size));
     world.setFillColor(sf::Color(50, 50, 50)); 
     world.setPosition(0, 0);
 
@@ -30,21 +36,46 @@ int main() {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && y>0)
             player.move(0, -speed);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && y<2000)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && y<w_size-50)
             player.move(0, speed);
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && x > 0)
             player.move(-speed, 0);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && x<2000)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && x<w_size-50)
             player.move(speed, 0);
+
+        if (player.getGlobalBounds().intersects(enemy.getGlobalBounds())) {
+            window.close(); // death 
+}
 
         view.setCenter(player.getPosition());
         window.setView(view);
 
+
+        float enemyX = enemy.getPosition().x;
+        float enemyY = enemy.getPosition().y;
+
+        float playerX = player.getPosition().x;
+        float playerY = player.getPosition().y;
+
+
+        float dx = playerX - enemyX;
+        float dy = playerY - enemyY;
+
+        float enemySpeed = 0.2f;
+
+        if (dx > 0) enemy.move(enemySpeed, 0);
+        if (dx < 0) enemy.move(-enemySpeed, 0);
+
+        if (dy > 0) enemy.move(0, enemySpeed);
+        if (dy < 0) enemy.move(0, -enemySpeed);
+
+
         window.clear();
 
-        window.draw(world);
+        window.draw(world);  // drawing world
+        window.draw(enemy);  // draw enemy
         window.draw(player); // drawing squere
 
         window.display();
